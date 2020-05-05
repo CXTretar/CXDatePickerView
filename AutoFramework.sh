fe049b4d80c8f1ac10543a2553a9fd1e5663f846
CXTretar <522796716@qq.com>
2020-05-05 11:20:59 +0800

    增加自动 trunk 文件。

---
+++ AutoFramework.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+#该脚本仅适用于cocoapods生成的framework静态库
+frameworkName='CXDatePickerView'
+#修改
+oldversion='0.1.9'
+#修改
+version='0.2.0'
+message=' 版本调整,修复BUG,增加秒,增加标题'
+
+#本地校验
+pod lib lint ${frameworkName}.podspec --verbose --use-libraries --allow-warnings
+#代码提交到服务器
+git add .
+git commit -a -m${version}${message}
+git tag -a $version -m${message}
+git push origin ${version}
+git push -u origin master 
+#修改version
+sed -i '' "s/${oldversion}/${version}/g" ${frameworkName}.podspec
+
+pod trunk push --allow-warnings ${frameworkName}.podspec
